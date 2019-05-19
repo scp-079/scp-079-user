9d82e858f29f56f9b0d00093cef2f1625c4d766f
Xiao <xiao@scp-079.org>
2019-05-19 13:10:35 +0000

    0.0.1

---
+++ plugins/glovar.py
@@ -0,0 +1,207 @@
+# SCP-079-USER - Invite and help other bots
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-USER.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import pickle
+from configparser import RawConfigParser
+from os import mkdir
+from os.path import exists
+from shutil import rmtree
+from typing import Dict, List, Set
+
+# Enable logging
+logger = logging.getLogger(__name__)
+
+# Init
+
+all_commands: List[str] = [
+    "config",
+    "print",
+    "user_config",
+    "version"
+]
+
+declared_message_ids: Dict[str, Dict[int, Set[int]]] = {
+    "ban": {},
+    "delete": {}
+}
+# declared_message_ids = {
+#     "ban": {
+#         -10012345678: {123}
+#     },
+#     "delete": {
+#         -10012345678: {124}
+#     }
+# }
+
+receivers_preview: List[str] = ["CLEAN", "LANG", "NOFLOOD", "NOPORN", "NOSPAM", "RECHECK"]
+
+sender = "USER"
+
+version: str = "0.0.1"
+
+# Read data from config.ini
+
+# [basic]
+prefix: List[str] = []
+prefix_str: str = "/!"
+
+# [bots]
+captcha_id: int = 0
+clean_id: int = 0
+lang_id: int = 0
+noflood_id: int = 0
+noporn_id: int = 0
+nospam_id: int = 0
+tip_id: int = 0
+user_id: int = 0
+warn_id: int = 0
+
+# [channels]
+debug_channel_id: int = 0
+exchange_channel_id: int = 0
+hide_channel_id: int = 0
+logging_channel_id: int = 0
+logging_channel_username: str = ""
+test_group_id: int = 0
+
+# [custom]
+default_group_link: str = ""
+project_link: str = ""
+project_name: str = ""
+reset_day: str = ""
+
+# [encrypt]
+key: str = ""
+password: str = ""
+
+try:
+    config = RawConfigParser()
+    config.read("config.ini")
+    # [basic]
+    prefix = list(config["basic"].get("prefix", prefix_str))
+    # [bots]
+    captcha_id = int(config["bots"].get("captcha_id", captcha_id))
+    clean_id = int(config["bots"].get("clean_id", clean_id))
+    lang_id = int(config["bots"].get("lang_id", lang_id))
+    noflood_id = int(config["bots"].get("noflood_id", noflood_id))
+    noporn_id = int(config["bots"].get("noporn_id", noporn_id))
+    nospam_id = int(config["bots"].get("nospam_id", nospam_id))
+    tip_id = int(config["bots"].get("tip_id", tip_id))
+    user_id = int(config["bots"].get("user_id", user_id))
+    warn_id = int(config["bots"].get("warn_id", warn_id))
+    # [channels]
+    debug_channel_id = int(config["channels"].get("debug_channel_id", debug_channel_id))
+    exchange_channel_id = int(config["channels"].get("exchange_channel_id", exchange_channel_id))
+    hide_channel_id = int(config["channels"].get("hide_channel_id", hide_channel_id))
+    logging_channel_id = int(config["channels"].get("logging_channel_id", logging_channel_id))
+    logging_channel_username = config["channels"].get("logging_channel_username", logging_channel_username)
+    test_group_id = int(config["channels"].get("test_group_id", test_group_id))
+    # [custom]
+    default_group_link = config["custom"].get("default_group_link", default_group_link)
+    project_link = config["custom"].get("project_link", project_link)
+    project_name = config["custom"].get("project_name", project_name)
+    reset_day = config["custom"].get("reset_day", reset_day)
+    # [encrypt]
+    key = config["encrypt"].get("key", key)
+    password = config["encrypt"].get("password", password)
+except Exception as e:
+    logger.warning(f"Read data from config.ini error: {e}", exc_info=True)
+
+# Check
+if (prefix == []
+        or captcha_id == 0
+        or clean_id == 0
+        or lang_id == 0
+        or noflood_id == 0
+        or noporn_id == 0
+        or nospam_id == 0
+        or tip_id == 0
+        or user_id == 0
+        or warn_id == 0
+        or debug_channel_id == 0
+        or exchange_channel_id == 0
+        or hide_channel_id == 0
+        or logging_channel_id == 0
+        or logging_channel_username in {"", "[DATA EXPUNGED]"}
+        or test_group_id == 0
+        or default_group_link in {"", "[DATA EXPUNGED]"}
+        or project_link in {"", "[DATA EXPUNGED]"}
+        or project_name in {"", "[DATA EXPUNGED]"}
+        or reset_day in {"", "[DATA EXPUNGED]"}
+        or key in {"", "[DATA EXPUNGED]"}
+        or password in {"", "[DATA EXPUNGED]"}):
+    raise SystemExit('No proper settings')
+
+bot_ids: Set[int] = {captcha_id, clean_id, lang_id, noflood_id, noporn_id, nospam_id, tip_id, user_id, warn_id}
+
+# Load data from pickle
+
+# Init dir
+try:
+    rmtree("tmp")
+except Exception as e:
+    logger.info(f"Remove tmp error: {e}")
+
+for path in ["data", "tmp"]:
+    if not exists(path):
+        mkdir(path)
+
+# Init ids variables
+
+bad_ids: Dict[str, Set[int]] = {
+    "channels": set(),
+    "users": set()
+}
+# bad_ids = {
+#     "channels": {-10012345678},
+#     "users": {12345678}
+# }
+
+except_ids: Dict[str, Set[int]] = {
+    "channels": set(),
+    "users": set()
+}
+# except_ids = {
+#     "channels": {-10012345678},
+#     "users": {12345678}
+# }
+
+# Load data
+file_list: List[str] = ["bad_ids", "except_ids"]
+for file in file_list:
+    try:
+        try:
+            if exists(f"data/{file}") or exists(f"data/.{file}"):
+                with open(f"data/{file}", "rb") as f:
+                    locals()[f"{file}"] = pickle.load(f)
+            else:
+                with open(f"data/{file}", "wb") as f:
+                    pickle.dump(eval(f"{file}"), f)
+        except Exception as e:
+            logger.error(f"Load data {file} error: {e}")
+            with open(f"data/.{file}", "rb") as f:
+                locals()[f"{file}"] = pickle.load(f)
+    except Exception as e:
+        logger.critical(f"Load data {file} backup error: {e}")
+        raise SystemExit("[DATA CORRUPTION]")
+
+# Start program
+copyright_text = (f"SCP-079-{sender} v{version}, Copyright (C) 2019 SCP-079 <https://scp-079.org>\n"
+                  "Licensed under the terms of the GNU General Public License v3 or later (GPLv3+)\n")
+print(copyright_text)
