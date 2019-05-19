ffe94822c69f258c7d544119d257a34adf26f3f0
Xiao <xiao@scp-079.org>
2019-05-19 03:10:33 +0000

    0.0.1

---
+++ README.md
@@ -0,0 +1,50 @@
+# SCP-079-USER
+
+This bot is used to invite and help other bots.
+
+## How to use
+
+See [this article](https://scp-079.org/user/).
+
+## To Do List
+
+- [x] Basic functions
+
+## Requirements
+
+- Python 3.6 or higher.
+- `requirements.txt` : APScheduler pyAesCrypt pyrogram[fast]
+
+## Files
+
+- plugins
+    - functions
+        - `channel.py` : Send messages to channel
+        - `etc.py` : Miscellaneous
+        - `file.py` : Save files
+        - `filters.py` : Some filters
+        - `group.py` : Functions about group
+        - `ids.py` : Modify id lists
+        - `image.py` : Functions about image
+        - `telegram.py` : Some telegram functions
+        - `timers.py` : Timer functions
+        - `user.py` : Functions about user
+    - handlers
+        - `callback.py` : Handle callbacks
+        - `command` : Handle commands
+        - `message.py`: Handle messages
+    - `glovar.py` : Global variables
+- `.gitignore` : Ignore
+- `config.ini.example` -> `config.ini` : Configures
+- `LICENSE` : GPLv3
+- `main.py` : Start here
+- `README.md` : This file
+- `requirements.txt` : Managed by pip
+
+## Contribute
+
+Welcome to make this project even better. You can submit merge requests, or report issues.
+
+## License
+
+Licensed under the terms of the [GNU General Public License v3](LICENSE).
---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# SCP-079-USER - Invite and help other bots
+# Copyright (C) 2019 SCP-079 <https://scp-079.org>
+#
+# This file is part of SCP-079-USER.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from pyrogram import Client
+
+from plugins import glovar
+from plugins.functions.timers import backup_files, reset_data, update_status
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.WARNING,
+    filename='log',
+    filemode='w'
+)
+
+logger = logging.getLogger(__name__)
+
+# Start
+app = Client(session_name="account")
+app.start()
+
+# Timer
+scheduler = BackgroundScheduler()
+scheduler.add_job(update_status, "cron", [app], minute=30)
+scheduler.add_job(backup_files, "cron", [app], hour=20)
+scheduler.add_job(reset_data, "cron", day=glovar.reset_day, hour=22)
+scheduler.start()
+
+# Hold
+app.idle()
