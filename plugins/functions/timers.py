a1043355e8d3ee2f7940b91d0db8c80aaabbc4ab
Xiao <xiao@scp-079.org>
2019-08-14 05:06:54 +0000

    - 测试

--- plugins/functions/timers.py
+++ plugins/functions/timers.py
@@ -103,6 +103,8 @@ def update_admins(client: Client) -> bool:
         try:
             should_leave = True
             admin_members = get_admins(client, gid)
+            # Test
+            logger.warning(gid)
             if admin_members and any([admin.user.is_self for admin in admin_members]):
                 glovar.admin_ids[gid] = {admin.user.id for admin in admin_members
                                          if not admin.user.is_bot and not admin.user.is_deleted}
